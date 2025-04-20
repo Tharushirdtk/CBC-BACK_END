e4c5446c9116f577820c5953ed992b7d54ab1311
Tharushirdtk <kavindirdtk@gmail.com>
2025-04-20 23:37:24 +0530

    delete product function 🗑️

--- controllers/productController.js
+++ controllers/productController.js
@@ -26,4 +26,35 @@ export function createProduct(req, res) {
           message: error,
         });
       });
-  }
+     }
+
+   export function getProducts(req, res) {
+        Product.find({}).then((products) => {
+          res.json(products);
+        });
+    }
+
+  export function deleteProduct(req, res) {
+    if (!isAdmin(req)) {
+      res.status(403).json({
+        message: "Please login as administrator to delete products",
+      });
+      return;
+    }
+  
+    const productId = req.params.productId;
+  
+    Product.deleteOne({ productId: productId })
+      .then(() => {
+        res.json({
+          message: "Product deleted",
+        });
+      })
+      .catch((error) => {
+        res.status(403).json({
+          message: error,
+        });
+      });
+    }
+
+    
