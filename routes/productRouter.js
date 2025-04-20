9669711d50477b613377577b7bfec03d768eb15b
Tharushirdtk <kavindirdtk@gmail.com>
2025-04-20 23:44:13 +0530

    complete the product router 🚚

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,8 +1,16 @@
 import express from "express";
+import { createProduct, deleteProduct, getProductById, getProducts, searchProducts, updateProduct } from '../controllers/productController.js';
 
 const productRouter = express.Router();
 
-productRouter.get
+productRouter.post("/",createProduct)
+
+productRouter.get("/",getProducts)
+productRouter.get("/search/:query",searchProducts)
+productRouter.get("/:productId",getProductById)
+productRouter.delete("/:productId",deleteProduct)
+productRouter.put("/:productId",updateProduct)
+
 
 
 
